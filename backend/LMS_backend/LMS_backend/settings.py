e4e1e8407c40a3b67696c35ec564633271c21166
vcube <mekalabhavani777@gmail.com>
2026-06-11 13:52:05 +0530

    Created LMS application structure

--- backend/LMS_backend/LMS_backend/settings.py
+++ backend/LMS_backend/LMS_backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'courses',
+    'enrollments',
+    'quizzes',
+    'certificates',
+    'recommendations',
 ]
 
 MIDDLEWARE = [
